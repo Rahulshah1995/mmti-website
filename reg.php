9efd010f4bcc09db17138bd9885394e5e0fdc669
Rahulshah1995 <85198995+Rahulshah1995@users.noreply.github.com>
2021-12-17 21:18:10 +0545

    Update reg.php

--- reg.php
+++ reg.php
@@ -1,1 +1,18 @@
+<?php
+date_default_timezone_set("Asisa");
+error_reporting(0);
+include('db.connection.php');
+$DBConn = dbConnect();
+if(1$DBConn)
+{
+  return false;
+}
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$phone = $_REQUEST['phone'];
+$message = $_REQUEST['message'];
 
+$Save_RegistrationSQL = "INSERT INTO registration(name,email,phone,message)" VALUES('".addslashes($name)."','".addslashes($email)."','".addslashes($phone)."','".addslashes($message)."')";
+$DBConn->query(Save_RegistrationSQL);
+header("Location:index.html?r=1");
+?>
