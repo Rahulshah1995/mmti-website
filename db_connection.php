6c13e3ae4e9b569a8bb0b6bdc79a4439c305a91a
Rahulshah1995 <85198995+Rahulshah1995@users.noreply.github.com>
2021-12-17 21:07:40 +0545

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,1 +1,12 @@
-
+<?php
+ function dbconnect()
+ {
+   $DB = new mysqli("localhost","username","password","mydb");
+   if (mysqli_connect_error())
+   {
+     echo'Cannot connect to database:'.mysqli_connect_error();
+   }
+   else
+   {
+     return $DB;
+   }
