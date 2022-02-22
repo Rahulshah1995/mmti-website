c4be3df8a679b4956999f2036aabef7340419d91
Rahulshah1995 <85198995+Rahulshah1995@users.noreply.github.com>
2022-02-22 21:29:17 +0545

    Update reg.php

--- reg.php
+++ reg.php
@@ -1,19 +1,54 @@
-<?php
-date_default_timezone_set("Asisa");
-error_reporting(0);
-include('db.connection.php');
-$DBConn = dbConnect();
-if(1$DBConn)
-{
-  return false;
-}
-$name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
-$address = $_REQUEST['address'];
-$phone = $_REQUEST['phone'];
-$message = $_REQUEST['message'];
-
-$Save_RegistrationSQL = "INSERT INTO registration(name,email,address,phone,message)" VALUES('".addslashes($name)."','".addslashes($email)."','".addslashes($address)."','".addslashes($phone)."','".addslashes($message)."')";
-$DBConn->query(Save_RegistrationSQL);
-header("Location:index.html?r=1");
-?>
+<!DOCTYPE html>
+<html>
+  
+<head>
+    <title>Insert Page page</title>
+</head>
+  
+<body>
+    <center>
+        <?php
+  
+        // servername => localhost
+        // username => root
+        // password => empty
+        // database name => mydb
+        $conn = mysqli_connect("localhost", "root", "", "mydb");
+          
+        // Check connection
+        if($conn === false){
+            die("ERROR: Could not connect. " 
+                . mysqli_connect_error());
+        }
+          
+        // Taking all 5 values from the form data(input)
+        $name =  $_REQUEST['name'];
+        $email = $_REQUEST['email'];
+        $address =  $_REQUEST['address'];
+        $phone = $_REQUEST['phone'];
+        $message = $_REQUEST['message'];
+          
+        // Performing insert query execution
+        // here our table name is college
+        $sql = "INSERT INTO registration  VALUES ('$name', 
+            '$email','$address','$phone','$message')";
+          
+        if(mysqli_query($conn, $sql)){
+            echo "<h3>data stored in a database successfully." 
+                . " Please browse your localhost php my admin" 
+                . " to view the updated data</h3>"; 
+  
+            echo nl2br("\n$name\n $email\n "
+                . "$address\n $phone\n $message");
+        } else{
+            echo "ERROR: Hush! Sorry $sql. " 
+                . mysqli_error($conn);
+        }
+          
+        // Close connection
+        mysqli_close($conn);
+        ?>
+    </center>
+</body>
+  
+</html>
