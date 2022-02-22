2db759adb5aebb8a9e8c040aee459fa37cbb4f38
Rahulshah1995 <85198995+Rahulshah1995@users.noreply.github.com>
2022-02-22 21:19:19 +0545

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,12 +1,26 @@
+
 <?php
- function dbconnect()
- {
-   $DB = new mysqli("localhost","username","password","mydb");
-   if (mysqli_connect_error())
-   {
-     echo'Cannot connect to database:'.mysqli_connect_error();
-   }
-   else
-   {
-     return $DB;
-   }
+  
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "mydb";
+  
+// Create connection
+$conn = new mysqli($servername, 
+    $username, $password, $dbname);
+  
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " 
+        . $conn->connect_error);
+}
+  
+$sqlquery = "INSERT INTO table VALUES 
+    ('John', 'john@example.com', 'Doe','9840012939','how r u')"
+  
+if ($conn->query($sql) === TRUE) {
+    echo "record inserted successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
